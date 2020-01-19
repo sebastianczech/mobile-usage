5cf086c0fee568992bc1f51d14e923801f016e7c
Sebastian Czech <sebaczech@gmail.com>
2020-01-19 10:07:12 +0100

    Dodanie uwierzytelniania i odczytu zuycia danych

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
 
-func checkNetgear(url string) string {
-	return "netgear: " + url
+	"github.com/gocolly/colly"
+)
+
+func checkNetgear(gatewayIP string, gatewayPassword string) string {
+	c := colly.NewCollector(
+		colly.AllowedDomains(gatewayIP),
+		// colly.Debugger(&debug.LogDebugger{}),
+	)
+
+	err := c.Post("http://"+gatewayIP+"/Forms/config", map[string]string{
+		"session.password": gatewayPassword,
+	})
+	if err != nil {
+		// log.Fatal(err)
+	}
+
+	c.OnHTML("span[class]", func(e *colly.HTMLElement) {
+		itemprop := e.Attr("class")
+		if itemprop == "m_datausage_dataTransferred" {
+			fmt.Println("Gateway mobile data usage:", e.Text)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting URL:", r.URL)
+	})
+
+	c.Visit("http://" + gatewayIP + "/index.html")
+
+	return "Finished checking mobile usage data on Netgear: " + gatewayIP
 }
 
 func main() {
-	fmt.Println(checkNetgear("http://10.212.194.1/index.html"))
+	argsWithoutProg := os.Args[1:]
+
+	fmt.Println(checkNetgear(argsWithoutProg[0], argsWithoutProg[1]))
 }
