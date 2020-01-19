cec560893d2dd9e211d7ae29146116e35d742dc4
Sebastian Czech <sebaczech@gmail.com>
2020-01-19 10:29:32 +0100

    Sprawdzanie stanu w nju

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
 )
 
 func checkNetgear(gatewayIP string, gatewayPassword string) string {
 	c := colly.NewCollector(
 		colly.AllowedDomains(gatewayIP),
-		// colly.Debugger(&debug.LogDebugger{}),
+		colly.Debugger(&debug.LogDebugger{}),
 	)
 
 	err := c.Post("http://"+gatewayIP+"/Forms/config", map[string]string{
 		"session.password": gatewayPassword,
 	})
 	if err != nil {
-		// log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	c.OnHTML("span[class]", func(e *colly.HTMLElement) {
@@ -36,8 +38,32 @@ func checkNetgear(gatewayIP string, gatewayPassword string) string {
 	return "Finished checking mobile usage data on Netgear: " + gatewayIP
 }
 
+func checkNju(njuLogin string, njuPassword string) string {
+	c := colly.NewCollector(
+		colly.AllowedDomains("https://www.njumobile.pl/"),
+		colly.Debugger(&debug.LogDebugger{}),
+	)
+
+	err := c.Post("https://www.njumobile.pl/logowanie", map[string]string{
+		"login-form":    njuLogin,
+		"password-form": njuPassword,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting URL:", r.URL)
+	})
+
+	c.Visit("https://www.njumobile.pl/")
+
+	return "Finished checking mobile usage data on Nju"
+}
+
 func main() {
 	argsWithoutProg := os.Args[1:]
 
 	fmt.Println(checkNetgear(argsWithoutProg[0], argsWithoutProg[1]))
+	fmt.Println(checkNju(argsWithoutProg[2], argsWithoutProg[3]))
 }
